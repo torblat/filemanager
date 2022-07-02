751ad8ce6c0d8ce32451a27e07338ffa94604d93
torblat <57583969+torblat@users.noreply.github.com>
2022-07-02 18:41:18 +0300

    Update filemanager.py

--- filemanager.py
+++ filemanager.py
@@ -12,24 +12,20 @@ while True:
         if ("touch") in ans:
             with open(str(filename), "w") as f:
                 print("File " + filename + " created")
-            screen_cleener()
         elif ("rm") in ans:
             os.remove(filename)
-            print("File was deleted")
-            screen_cleener()
+            print("File was deleted")  
         elif ("cd") in ans:
             os.chdir(filename)
-            screen_cleener()
         elif ("mkdir") in ans:
-            os.mkdir(filename)
-            screen_cleener()
+            os.mkdir(filename) 
         elif ("ls") in ans:
             print(os.listdir(os.path.abspath(os.curdir)))
-            screen_cleener()
         else:
             print("ERROR")
             print("You enter wrong command")
     except Exception as e:
         print("ERROR!")
         print(e)
-        screen_cleener()
+    screen_cleener()
+        
